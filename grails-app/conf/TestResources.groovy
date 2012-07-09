4831b22c71ab795c124d810f42d3d7d38d149d4e
musketyr <vladimir@orany.cz>
2012-07-09 12:41:00 +0300

    Update master

--- grails-app/conf/TestResources.groovy
+++ grails-app/conf/TestResources.groovy
@@ -1,9 +1,9 @@
 modules = {
-	'test' {
-		dependsOn 'jasmine'
-		resource url: 'plugins/js/Player.js', disposition: 'head'
-		resource url: 'plugins/js/Song.js', disposition: 'head'
-		resource url: 'plugins/js/SpecHelper.js', disposition: 'head'
-		resource url: 'plugins/js/PlayerSpec.js', disposition: 'head'
-    }
+  'test' {
+    dependsOn 'jasmine'
+    resource url: 'plugins/js/Player.js', disposition: 'head'
+    resource url: 'plugins/js/Song.js', disposition: 'head'
+    resource url: 'plugins/js/SpecHelper.js', disposition: 'head'
+    resource url: 'plugins/js/PlayerSpec.js', disposition: 'head'
+  }
 }
